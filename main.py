d92f0f4cb9c1b15a49e81b59d01bcc3bda85dfbc
Shrikant Pawar <36473243+sspawar23993@users.noreply.github.com>
2025-01-24 20:36:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+from data_model import Water
+
+app=FastAPI(
+    title="water potability prediction",
+    description="Predicting water Potability"
+)
+
+with open(r"C:\Users\sspaw\ml_pipeline\model.pkl","rb") as f:
+    model=pickle.load(f)
+
+@app.get("/")
+def index():
+    return "welcome to water potability prediction fastapi"
+
+
+@app.post("/predict")
+def model_predict(water: Water):
+    sample=pd.DataFrame({
+        'ph': [water.ph],
+    'Hardness' : [water.Hardness],
+    'Solids' : [water.Solids],
+    'Chloramines' : [water.Chloramines],
+    'Sulfate' : [water.Sulfate ],
+    'Conductivity' : [water.Conductivity],
+    'Organic_carbon' : [water.Organic_carbon],
+    'Trihalomethanes' : [water.Trihalomethanes],
+    'Turbidity' : [water.Turbidity]
+    })
+
+    predicted_value=model.predict(sample)
+
+    if predicted_value==1:
+        return "water is consumable"
+    else:
+        return "water is not consumable"
+    
+
